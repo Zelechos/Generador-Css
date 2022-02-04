69358943d3f77e3603ee9ea687c6de3750ce25c5
Pragmatic Coder <zelechos@hotmail.com>
2022-02-04 04:02:11 -0400

    v0.1.5

--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
-'use strict';
 
+'use strict';
+import stylesData from "./model.js";
 window.addEventListener('load', ()=> {
     
     copyStyle();
-    generatorDataset();    
+    obtenerHTML();
     
 });
 
@@ -19,11 +20,11 @@ const loadModel = async ()=> {
 
 
 
-const generatorDataset = ()=>{
-    const styleArea = document.querySelector('#code');
-
-    // Aqui le pasamos la etiqueta queramos
-    styleArea.innerHTML= tableOfPromos(50);
+const generatorCss = (data)=>{
+    const styleArea = document.querySelector('#code-generator');
+    setTimeout(()=>{
+        styleArea.innerHTML= generator(data);
+    } , 4000);
 }
 
 // Subrutina para generar los colores CSS
@@ -42,7 +43,7 @@ const generatorColors = () =>{
 const copyStyle = () => {
     const button = document.querySelector('#btnCopy');
     const alert = document.querySelector("#aviso");
-    const input = document.querySelector('#code');
+    const input = document.querySelector('#code-generator');
     
     button.addEventListener('click',()=>{
         input.classList.toggle('animation');
@@ -53,7 +54,54 @@ const copyStyle = () => {
         setTimeout(()=> alert.innerHTML = "" , 4000);
     });
     input.classList.toggle('animation');
+}
 
+
+const obtenerHTML = ()=>{
+    const btn = document.querySelector("#btnGenerator");
+    const input = document.querySelector("#code");
+    const alert = document.querySelector("#aviso1");
+
+    btn.addEventListener('click', ()=>{
+        input.classList.toggle('animation');
+        input.focus();
+        alert.innerHTML = "Generando Codigo";
+        setTimeout(()=> alert.innerHTML = "" , 4000);
+        console.log(input.value);
+
+        let selector = selectorCss(input.value);
+        console.log("ests ",selector);
+        generatorCss(selector);
+        
+    });
+}
+
+// obtenemos selector
+const selectorCss = (value)=>{
+
+    for (const key in stylesData.styles) {
+        console.error(stylesData.styles[key].selector);
+        
+        // console.error(stylesData[key].selector);
+
+        if (value.includes(stylesData.styles[key].selector)) {
+            return stylesData.styles[key].selector;
+        }
+        // console.log(stylesData[key].selector);
+    }
+
+    console.warn((value.substr(value.indexOf('"')+1), (value.indexOf('">')-value.indexOf('"'))));
+    return value.substr((value.indexOf('"')+1), (value.indexOf('">')-(value.indexOf('"')+1)));
+}
+
+// generamos estilos
+const generator = (data)=>{
+    for (const key in stylesData.styles) {
+        if (stylesData.styles[key].selector == data) {
+            return stylesData.styles[key].style;
+        }
+    }
+    return random(data);
 }
 
 
@@ -82,34 +130,72 @@ const copyStyle = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const random = (selector)=>{
+    let sel = `${selector} {
+      position: relative;
+      display: inline-block;
+      width: 40px;
+      height: 23px;
+      background-color: ${generatorColors()};
+      border-radius: 20px;
+      transition: all 0.3s
+    }
+    .switch:after {
+      content: '';
+      position: absolute;
+      width: 21px;
+      height: 21px;
+      border-radius: 20px;
+      background-color: ${generatorColors()};
+      top: 1px;
+      left: 1px;
+      transition: all 0.3s
+    }
+    `;
+  
+    let sel1 = `.${selector} {
+      width: 40px;
+      width: 40px;
+      width: 40px;
+      width: 40px;
+      width: 40px;
+      border-radius: 20px;
+      transition: all 0.3s
+    }
+    .${selector} {
+      content: '';
+      position: absolute;
+      position: absolute;
+      position: absolute;
+      position: absolute;
+      background-color: ${generatorColors()};
+      background-color: ${generatorColors()};
+      background-color: ${generatorColors()};
+      background-color: ${generatorColors()};
+    }
+    `;
+  
+    let sel2 = `.${selector}:hover {
+      border-radius: 2rem;
+    box-shadow: 1px 1px 0 1px ${generatorColors()}, -1px 0 28px 0 ${generatorColors()}, 28px 28px 28px 0 rgba(34, 33, 81, .25);
+    transition: 0.4s ease-in-out transform, 0.3s ease-in-out box-shadowborder-radius: 2rem;
+    box-shadow: 1px 1px 0 1px ${generatorColors()}, -1px 0 28px 0 ${generatorColors()}, 28px 28px 28px 0 rgba(34, 33, 81, .25);
+    transition: 0.4s ease-in-out transform, 0.3s ease-in-out box-shadowborder-radius: 2rem;
+    box-shadow: 1px 1px 0 1px ${generatorColors()}, -1px 0 28px 0 ${generatorColors()}, 28px 28px 28px 0 rgba(34, 33, 81, .25);
+    }
+    .${selector}:after {
+      content: '';
+      content: '';
+      content: '';
+    }
+    `;
+  
+    const error = [sel, sel1, sel2];
+    
+    let numberAleatorio = Math.round(Math.random() * 2);//0 al 1000
+  
+    return error[numberAleatorio];
+}
 
 const load = async ()=> {
     console.log("Cargando el Modelo ...");
