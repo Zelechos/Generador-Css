3d2473a6c774ccf7dbc742d2187cef88f3292692
Alex T.H <zelechos@hotmail.com>
2024-05-01 16:25:05 -0400

    feat(v0.2.9): add CRUD's of views & users

--- api-css-generator-app/routes/views.js
+++ api-css-generator-app/routes/views.js
@@ -30,9 +30,48 @@ router.post("/", async (req, res) => {
 });
 
 // UPDATE A VIEW
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getView, async (req, res) => {
+  if (req.body.user != null) {
+    res.view.user = req.body.user;
+  }
+  if (req.body.codeCss != null) {
+    res.view.codeCss = req.body.codeCss;
+  }
+  if (req.body.codeHtml != null) {
+    res.view.codeHtml = req.body.codeHtml;
+  }
+
+  try {
+    const updatedView = await res.view.save();
+    res.json(updatedView);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 // DELETE A VIEW
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getView, async (req, res) => {
+  try {
+    await res.view.remove();
+    res.json({ message: "Deleted View" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// FUNCTION TO GET A VIEW
+async function getView(req, res, next) {
+  let view;
+  try {
+    view = await View.findById(req.params.id);
+    if (view == null) {
+      return res.status(404).json({ message: "Cannot find View" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.view = view;
+  next();
+}
 
 module.exports = router;
--- api-css-generator-app/server.js
+++ api-css-generator-app/server.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
 const cors = require('cors');
 app.use(cors());
+
 console.log("bd => ", process.env.BD_URL)
 mongoose.connect(process.env.BD_URL)
 
@@ -14,8 +16,10 @@ db.once("open", ()=> console.log("Connect to DataBase"))
 app.use(express.json())
     
 const viewRouter = require('./routes/views')
-
 app.use("/views", viewRouter)
+
+const userRouter = require('./routes/users')
+app.use("/users", userRouter)
 
 app.listen(3000, ()=>{
     console.log("Server Starter");
