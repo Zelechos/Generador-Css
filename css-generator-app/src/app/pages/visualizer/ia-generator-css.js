bcc74d77d3cf6a4741448ec854b2cc61c40fdbbc
Alex T.H. <zelechos@hotmail.com>
2024-06-13 02:40:12 -0400

    feat(v0.5.6): add corrections in docs & add button of color palette in app web

--- css-generator-app/src/app/pages/visualizer/ia-generator-css.js
+++ css-generator-app/src/app/pages/visualizer/ia-generator-css.js
@@ -1,14 +1,26 @@
 "use strict";
 import { main } from "./api-model.js";
 
-export const getGeneratedCSSStyle = async (codeHtml) => {
+export const getGeneratedCSSStyle = async (codeHtml, color) => {
   // Model Implementation
   let response = "";
   if (codeHtml != null) {
-    let realRequest = `Generame un solo estilo CSS para este codigo HTML : ${codeHtml}`;
+    let realRequest = `Generame con la paleta ${randomPalette()} de colores en base al color ${color} y implementa esta paleta de colores en un solo estilo CSS para este codigo HTML sin generar codigo HTML, y que el estilo css generado tenga un enfoque de estilo Responsive Design : ${codeHtml}`;
     console.warn("GROK PREGUNTA ==> ", realRequest);
     response = await main(realRequest);
     console.warn(response);
   }
   return response;
 };
+
+const randomPalette= ()=>{
+  const palettes = ["Análoga","Monocromática","Complementaria", "Triádica", "Tetrádica"];
+  const randomIndex = Math.floor(Math.random() * palettes.length);
+  return palettes[randomIndex]
+}
+
+const randomApproach= ()=>{
+  const approachs = ["Minimalista","Flat Design","Material Design", "Skeuomorphic Design", "Neumorphism (Soft UI)", "Responsive Design", "Retro/Vintage", "Tipográfico", "Asimétrico"];
+const randomIndex = Math.floor(Math.random() * approachs.length);
+  return approachs[randomIndex]
+}
